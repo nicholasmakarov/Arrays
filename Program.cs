539d9352ff2352cc7cdc4f69d6aff1fb65586dd9
nicholasmakarov <122953985+nicholasmakarov@users.noreply.github.com>
2023-02-09 22:30:32 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Author: Nicholas Makarov
+Date: 2/8/2023
+Comments: This C# Application code demonstrates the use of arrays utilizing the fibonacci sequence 
+*/
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+            for (int i = 2; i < 25; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Fibonacci number ({0}) = {1}", i, fibonacci[i]);
+            }
+        }
+            }
+        }
+    
